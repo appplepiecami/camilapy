9ecfac5536b87c31bac4765e70931e6db0d16c02
Camila Franci <cmlfranci@gmail.com>
2021-05-30 18:17:27 -0500

    if else

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 print('--------CAMIPY--------')
 
-numero = input('Ingrese algo:')
+numero = int(input('Ingrese algo:'))
 print(numero)
+
+if numero == 1:
+    print('GANASTES')
+else:
+    print('PERDISTE')

a2fea7e51ff8f78dba17edc6e71f60f25e8a9f31
MATT-HP\matia <matias.franci@gmail.com>
2021-05-30 18:26:36 -0500

    Con emoji

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 print('--------CAMIPY--------')
 
 numero = int(input('Ingrese algo:'))
-print(numero)
+print(f'Ud. ingresó el número: {numero}')
+
+frutas = ["banana", "fresa", "kiwi"]
 
 if numero == 1:
     print('GANASTES')
 else:
     print('PERDISTE')
+
+if numero <= 100:
+    for i in range(numero):
+        print(f'{i+1}-Besistos')
+else:
+    print("no soy un loco😛")
+
+print('-----------------')
+for fruta in frutas:
+    print(f'Mi fruta es: {fruta}')
+
+
+print('--------FIN--------')
